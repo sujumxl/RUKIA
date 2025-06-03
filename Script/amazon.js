0756b1eb72f04d7698d14faf5b813c4ecab46e81
sujumxl <hajjsuleiman360@gmail.com>
2025-06-03 00:28:24 -0700

    Update amazon.js

--- Script/amazon.js
+++ Script/amazon.js
@@ -15,7 +15,7 @@ products.forEach(
 
                 <div class="product-rating-container">
                     <img class="product-rating-stars"
-                    src="images/ratings/rating-${product.rating.stars * 10}.png">
+                    src="ratings/rating-${product.rating.stars * 10}.png">
                     
                     <div class="product-rating-count link-primary">
                     ${product.rating.count}
